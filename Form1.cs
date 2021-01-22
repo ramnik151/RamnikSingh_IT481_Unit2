d388f0d3b038738a1001b442576ecf0e11fa113e
Ramnik singh <ramnik_151@hotmail.com>
2021-01-22 11:36:56 +0400

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+ 
+
+namespace RamnikSingh_IT481_Unit2
+{
+    public partial class Form1 : Form
+
+    {
+        db1 database;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new EventHandler(button1_Click);
+            button2.Click += new EventHandler(button2_Click);
+            button3.Click += new EventHandler(button3_Click);
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            database = new db1("Persist Security Info=False;Integrated Security=true;Initial Catalog=Northwind;Server=RAMNIK-SURFACE\\SQLEXPRESS");
+            MessageBox.Show("Connection information sent");
+             
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            database = new db1("Persist Security Info=False;Integrated Security=true;Initial Catalog=Northwind;Server=RAMNIK-SURFACE\\SQLEXPRESS");
+            String count = database.getCustomerCount();
+            MessageBox.Show(count, "Customer Count");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            database = new db1("Persist Security Info=False;Integrated Security=true;Initial Catalog=Northwind;Server=RAMNIK-SURFACE\\SQLEXPRESS");
+            string names = database.getCompanyNames();
+            MessageBox.Show(names, "company names");
+        }
+ 
+    }
+}
---
+++ db1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace RamnikSingh_IT481_Unit2
+{
+    class db1
+	{
+		string connectionString;
+		SqlConnection cnn;
+		public db1()
+		{
+			connectionString= "Persist Security Info=False;Integrated Security=true;Initial Catalog=Northwind;Server=RAMNIK-SURFACE\\SQLEXPRESS";
+		}
+		public db1(string conn)
+		{
+			connectionString = conn;
+		}
+		public string getCustomerCount()
+		{
+			Int32 count = 0;
+			cnn = new SqlConnection(connectionString);
+			cnn.Open();
+			String countQuery = "select count(*) from customers;";
+			SqlCommand cmd = new SqlCommand(countQuery, cnn);
+
+			try
+			{
+				count = Convert.ToInt32(cmd.ExecuteScalar());
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			return count.ToString();
+		}
+
+		public string getCompanyNames()
+		{
+			string names = "None";
+			SqlDataReader dataReader;
+
+			cnn = new SqlConnection(connectionString);
+			cnn.Open();
+			string countQuery = "select Companyname from customers;";
+			SqlCommand cmd = new SqlCommand(countQuery, cnn);
+			dataReader = cmd.ExecuteReader();
+			while (dataReader.Read())
+			{
+				try
+				{
+					names = names + dataReader["CompanyName"]+ "\n";
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+				}
+
+			}
+
+			return names;
+
+		}
+
+	}
+	
+}
